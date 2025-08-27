e1293d37012b38118228583697134f298cb08680
shubhapramanik07 <shubhapramanik07@gmail.com>
2025-08-27 23:27:41 +0530

    initial commit..

--- app.py
+++ app.py
@@ -101,7 +101,16 @@
 #### http verb GET and POST....
 
 
-from flask import Flask, redirect, url_for,render_template
+###next we have-     Jinja2 template engine
+'''
+{%...%} condition , for loop we can use it.
+{{   }} expressions to print output
+{#  Comment  # } this is  for comments
+'''
+
+#.............................................
+
+from flask import Flask, redirect, url_for,render_template,request
 
 
 app = Flask(__name__)
@@ -123,7 +132,15 @@ def welcome():
 
 
 def success(score):
-    return render_template('success.html', score=score)
+
+    # res = ""
+    # if score >= 50:
+    #     res = "PASS"
+    # else:
+    #     res = "FAIL"
+
+
+    return render_template('result.html', result=score)
 
 
 
@@ -147,7 +164,25 @@ def results(marks):
 
 #this will come after the submit...
 
-# @app.route 
+@app.route('/submit', methods=['POST','GET'])
+def submit():
+    total_score = 0
+    if request.method =="POST":
+        science = float(request.form['science'])
+        maths = float(request.form['maths'])
+        c = float(request.form['c'])
+        data_science = float(request.form['data_science'])
+        total_score = (science+maths+c+data_science)/4
+
+
+    # res = ""
+    # if total_score >= 50:
+    #     res = "success"
+    # else:
+    #     res = "fail"
+    return redirect(url_for('success',score=total_score))
+
+
 
 
 if __name__ == '__main__':
